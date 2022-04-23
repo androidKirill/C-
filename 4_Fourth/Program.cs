983c03ab89f284223d4c8b05c9fe00ff9e80946a
Kirill <mariwitch@mail.ru>
2022-04-23 11:26:21 +0300

    Ask

---
+++ 4_Fourth/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name:");
+string user_name = Console.ReadLine();
+
+if (user_name.ToLower() == "m")
+{
+    Console.WriteLine("Ouh my M!");    
+}
+else
+{
+    Console.Write("Hello " + user_name);    
+}
